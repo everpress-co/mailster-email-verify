6fa432c2afd4a864ebdffee175baf666e4fd3e83
Xaver <xaver@everpress.co>
2024-05-27 15:00:28 +0200

    updated tested up to

--- mailster-email-verify.php
+++ mailster-email-verify.php
@@ -3,7 +3,7 @@
 Plugin Name: Mailster Email Verify
 Plugin URI: https://mailster.co/?utm_campaign=wporg&utm_source=wordpress.org&utm_medium=plugin&utm_term=Email+Verify
 Description: Verifies your subscribers email addresses
-Version: 1.6
+Version: 1.6.0
 Author: EverPress
 Author URI: https://mailster.co
 Text Domain: mailster-email-verify
@@ -11,9 +11,9 @@ License: GPLv2 or later
 */
 
 
-define( 'MAILSTER_EMAILVERIFY_VERSION', '1.6' );
+define( 'MAILSTER_EMAILVERIFY_VERSION', '1.6.0' );
 define( 'MAILSTER_EMAILVERIFY_REQUIRED_VERSION', '2.3' );
 define( 'MAILSTER_EMAILVERIFY_FILE', __FILE__ );
 
-require_once dirname( __FILE__ ) . '/classes/emailverify.class.php';
+require_once __DIR__ . '/classes/emailverify.class.php';
 new MailsterEmailVerify();
